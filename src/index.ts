d0308c04302cf108ee553a90ebe8456317f62192
Christoph Kruppe <c.kruppe@nfq.de>
2023-07-04 16:58:58 +0200

    fix(Types): Add jsonification of return type for api requests (#13)

--- src/index.ts
+++ src/index.ts
@@ -12,3 +12,4 @@ export {
 export {HTTP_METHODS, HTTP_STATUS} from './utils/constants';
 export {fetcher, mutationFetcher} from './utils/fetchers';
 export type {MutationRepoArgs, RepositoryError, RequestError} from './utils/fetchers';
+export type {Jsonify} from './frontend';
